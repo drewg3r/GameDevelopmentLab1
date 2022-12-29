57be034a9af8bed0097ba2548511927b3c3cf601
Andrew <drewg3r@gmail.com>
2022-12-29 21:12:54 +0100

    Initial commit

---
+++ src/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+       float h = Input.GetAxisRaw("Horizontal");
+       float v = Input.GetAxisRaw("Vertical");
+       float speed = 0.1F;
+       
+       gameObject.transform.position = new Vector2 (transform.position.x + (h * speed), 
+          transform.position.y + (v * speed));
+    }
+}
